92142a7d625f362b71f2b8510e839bd0a8ab4ab5
Yevhen Kyniev <crm.33850@gmail.com>
2025-02-02 02:40:45 +0200

    update: add default entities

--- FinTrack.API/ApplicationDbContext.cs
+++ FinTrack.API/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 public class ApplicationDbContext : IdentityDbContext
@@ -7,5 +8,41 @@ public class ApplicationDbContext : IdentityDbContext
     public DbSet<Transaction> Transactions { get; set; }
     public DbSet<Report> Reports { get; set; }
     public DbSet<UserSettings> UserSettings { get; set; }
-    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        var adminRoleId = Guid.Parse("11111111-1111-1111-1111-111111111111");
+        var adminRole = new IdentityRole
+        {
+            Id = adminRoleId.ToString(),
+            Name = "Admin",
+            NormalizedName = "ADMIN"
+        };
+
+        var adminUserId = Guid.Parse("22222222-2222-2222-2222-222222222222");
+        var adminUser = new IdentityUser
+        {
+            Id = adminUserId.ToString(),
+            UserName = "admin",
+            NormalizedUserName = "ADMIN",
+            Email = "admin@example.com",
+            NormalizedEmail = "ADMIN@EXAMPLE.COM",
+            EmailConfirmed = true
+        };
+
+        var passwordHasher = new PasswordHasher<IdentityUser>();
+        adminUser.PasswordHash = passwordHasher.HashPassword(adminUser, "Admin123!");
+
+        var adminUserRole = new IdentityUserRole<string>
+        {
+            UserId = adminUserId.ToString(),
+            RoleId = adminRoleId.ToString()
+        };
+
+        builder.Entity<IdentityRole>().HasData(adminRole);
+        builder.Entity<IdentityUser>().HasData(adminUser);
+        builder.Entity<IdentityUserRole<string>>().HasData(adminUserRole);
+    }
+
 }
---
+++ FinTrack.API/Migrations/20250202003934_Add User And Role Initial Entities.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FinTrack.API.Migrations
+{
+    public partial class AddUserAndRoleInitialEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    FilePath = table.Column<string>(type: "text", nullable: false),
+                    GeneratedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Category = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserSettings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    Language = table.Column<string>(type: "text", nullable: false),
+                    Preferences = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSettings", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "11111111-1111-1111-1111-111111111111", null, "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "22222222-2222-2222-2222-222222222222", 0, "f6dcba0e-d416-4c20-8ecc-9d27c4c60076", "admin@example.com", true, false, null, "ADMIN@EXAMPLE.COM", "ADMIN", "AQAAAAIAAYagAAAAENzhdtMLubDDUqmKK9ibPTXF/ICmBGOCsAILDH5nrdxjV4SQXrRMTrDRpCI7Q1LFPw==", null, false, "2524695c-fe2f-45f3-b72d-0b4077052f01", false, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "11111111-1111-1111-1111-111111111111", "22222222-2222-2222-2222-222222222222" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reports");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "UserSettings");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "11111111-1111-1111-1111-111111111111", "22222222-2222-2222-2222-222222222222" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "11111111-1111-1111-1111-111111111111");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "22222222-2222-2222-2222-222222222222");
+        }
+    }
+}
