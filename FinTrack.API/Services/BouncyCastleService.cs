a0fcf49c626ea1c52334a8b91c6afc4fb7b35196
Yevhen Kyniev <skezze@DESKTOP-4BTJVDM>
2025-03-16 02:12:05 +0200

    initialize signing methods

--- FinTrack.API/Services/BouncyCastleService.cs
+++ FinTrack.API/Services/BouncyCastleService.cs
@@ -2,6 +2,8 @@
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Security;
 using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Crypto.Signers;
 
 namespace FinTrack.API.Services.Interfaces
 {
@@ -43,6 +45,41 @@ namespace FinTrack.API.Services.Interfaces
             {
                 PemWriter pemWriter = new PemWriter(writer);
                 pemWriter.WriteObject(key);
+            }
+        }
+
+        public string SignDocument(string filePath)
+        {
+            AsymmetricKeyParameter privateKey = LoadPrivateKey("private.pem");
+
+            byte[] data = File.ReadAllBytes(filePath);
+
+            byte[] signature = SignData(data, privateKey);
+
+            File.WriteAllBytes("signature.sig", signature);
+            var fileName = Path.GetFileName(filePath);
+            return $"{pemDirectory}\\{fileName}-signature-{new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds()}.sig";
+        }
+
+        public static byte[] SignData(byte[] data, AsymmetricKeyParameter privateKey)
+        {
+            var digest = new Sha256Digest();
+            digest.BlockUpdate(data, 0, data.Length);
+            byte[] hashedData = new byte[digest.GetDigestSize()];
+            digest.DoFinal(hashedData, 0);
+
+            var signer = new RsaDigestSigner(new Sha256Digest());
+            signer.Init(true, privateKey);
+            signer.BlockUpdate(hashedData, 0, hashedData.Length);
+            return signer.GenerateSignature();
+        }
+
+        public static AsymmetricKeyParameter LoadPrivateKey(string path)
+        {
+            using (TextReader reader = new StreamReader(path))
+            {
+                var pemReader = new PemReader(reader);
+                return ((AsymmetricCipherKeyPair)pemReader.ReadObject()).Private;
             }
         }
     }
